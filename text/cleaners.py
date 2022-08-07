5b2fc99c276a2349276e6fc18110cdcae6fbcf3c
FrancisHu <87693204+Francis-Komizu@users.noreply.github.com>
2022-08-07 10:40:18 +0800

    Update cleaners.py

--- text/cleaners.py
+++ text/cleaners.py
@@ -156,3 +156,110 @@ def chinese_tokenization_cleaners(text_ch):
   romaji = romaji[:-1]
 
   return romaji
+
+
+def japanese_cleaners(text):
+  '''Pipeline for Japanese text.'''
+  sentences = re.split(_japanese_marks, text)
+  marks = re.findall(_japanese_marks, text)
+  text = ''
+  for i, mark in enumerate(marks):
+    if re.match(_japanese_characters, sentences[i]):
+      text += pyopenjtalk.g2p(sentences[i], kana=False).replace('pau','').replace(' ','')
+    text += unidecode(mark).replace(' ','')
+  if re.match(_japanese_characters, sentences[-1]):
+      text += pyopenjtalk.g2p(sentences[-1], kana=False).replace('pau','').replace(' ','')
+  if re.match('[A-Za-z]',text[-1]):
+    text += '.'
+  return text
+
+def japanese_tokenization_cleaners(text):
+  '''Pipeline for tokenizing Japanese text.'''
+  words = []
+  for token in tokenizer.tokenize(text):
+    if token.phonetic!='*':
+      words.append(token.phonetic)
+    else:
+      words.append(token.surface)
+  text = ''
+  for word in words:
+    if re.match(_japanese_characters, word):
+      if word[0] == '\u30fc':
+        continue
+      if len(text)>0:
+        text += ' '
+      text += pyopenjtalk.g2p(word, kana=False).replace(' ','')
+    else:
+      text += unidecode(word).replace(' ','')
+  if re.match('[A-Za-z]',text[-1]):
+    text += '.'
+  return text
+
+def japanese_accent_cleaners(text):
+  '''Pipeline for notating accent in Japanese text.'''
+  '''Reference https://r9y9.github.io/ttslearn/latest/notebooks/ch10_Recipe-Tacotron.html'''
+  sentences = re.split(_japanese_marks, text)
+  marks = re.findall(_japanese_marks, text)
+  text = ''
+  for i, sentence in enumerate(sentences):
+    if re.match(_japanese_characters, sentence):
+      text += ':'
+      labels = pyopenjtalk.extract_fullcontext(sentence)
+      for n, label in enumerate(labels):
+        phoneme = re.search(r'\-([^\+]*)\+', label).group(1)
+        if phoneme not in ['sil','pau']:
+          text += phoneme
+        else:
+          continue
+        n_moras = int(re.search(r'/F:(\d+)_', label).group(1))
+        a1 = int(re.search(r"/A:(\-?[0-9]+)\+", label).group(1))
+        a2 = int(re.search(r"\+(\d+)\+", label).group(1))
+        a3 = int(re.search(r"\+(\d+)/", label).group(1))
+        if re.search(r'\-([^\+]*)\+', labels[n + 1]).group(1) in ['sil','pau']:
+          a2_next=-1
+        else:
+          a2_next = int(re.search(r"\+(\d+)\+", labels[n + 1]).group(1))
+        # Accent phrase boundary
+        if a3 == 1 and a2_next == 1:
+          text += ' '
+        # Falling
+        elif a1 == 0 and a2_next == a2 + 1 and a2 != n_moras:
+          text += ')'
+        # Rising
+        elif a2 == 1 and a2_next == 2:
+          text += '('
+    if i<len(marks):
+      text += unidecode(marks[i]).replace(' ','')
+  if re.match('[A-Za-z]',text[-1]):
+    text += '.'
+  return text
+
+def japanese_phrase_cleaners(text):
+  '''Pipeline for dividing Japanese text into phrases.'''
+  sentences = re.split(_japanese_marks, text)
+  marks = re.findall(_japanese_marks, text)
+  text = ''
+  for i, sentence in enumerate(sentences):
+    if re.match(_japanese_characters, sentence):
+      if text != '':
+        text += ' '
+      labels = pyopenjtalk.extract_fullcontext(sentence)
+      for n, label in enumerate(labels):
+        phoneme = re.search(r'\-([^\+]*)\+', label).group(1)
+        if phoneme not in ['sil','pau']:
+          text += phoneme
+        else:
+          continue
+        a3 = int(re.search(r"\+(\d+)/", label).group(1))
+        if re.search(r'\-([^\+]*)\+', labels[n + 1]).group(1) in ['sil','pau']:
+          a2_next=-1
+        else:
+          a2_next = int(re.search(r"\+(\d+)\+", labels[n + 1]).group(1))
+        # Accent phrase boundary
+        if a3 == 1 and a2_next == 1:
+          text += ' '
+    if i<len(marks):
+      text += unidecode(marks[i]).replace(' ','')
+  if re.match('[A-Za-z]',text[-1]):
+    text += '.'
+  return text
